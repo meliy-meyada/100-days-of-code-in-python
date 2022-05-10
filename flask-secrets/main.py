26dca9229374f83a3ba02f6f3728446228662a9e
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-05-10 17:04:00 +0700

    challenge:2

--- flask-secrets/main.py
+++ flask-secrets/main.py
@@ -1,13 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
-from wtforms import  StringField
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
 
 class LoginForm(FlaskForm):
-    email = StringField('Email')
-    password = StringField('Password')
+    email = StringField(label='Email', validators=[DataRequired(), Email("This field requires a valid email address")])
+    password = PasswordField(label='Password', validators=[DataRequired()])
+    submit = SubmitField(label='Log In')
 
 app = Flask(__name__)
 app.secret_key = "\x06\x9fw\xac>\xff\xe8j\x07\x00\x14\xc6Z\x03\xac\xd2Rv\x9d\x90\x87\x82\xd3\x83'"
+
 
 
 @app.route("/")
@@ -15,10 +18,17 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     login_form = LoginForm()
-    return render_template('login.html', form=login_form)
+    if request.method == 'POST' and login_form.validate():
+        if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
+            return  render_template("success.html")
+        else:
+            return render_template("denied.html")
+    return render_template("login.html", form=login_form)
+
+
 
 
 if __name__ == '__main__':
