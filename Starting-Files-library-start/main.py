3dfaa0eedec227bc63f197d93ff5f2100fd47270
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-05-13 15:51:19 +0700

    Let Started Challenge Days 63

---
+++ Starting-Files-library-start/main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import  StringField, SubmitField
+
+
+app = Flask(__name__)
+Bootstrap(app)
+
+
+all_books = []
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route("/add")
+def add():
+    return render_template("add.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
