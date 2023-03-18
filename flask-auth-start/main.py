dd9118d88190279ccf72525bfdae36a6f947a319
Meliy <meyada.saisan@gmail.com>
2023-03-18 11:42:11 +0700

    Hashing Passwords using Werkzeug

--- flask-auth-start/main.py
+++ flask-auth-start/main.py
@@ -29,6 +29,11 @@ def home():
 @app.route('/register', methods=["GET", "POST"])
 def register():
     if request.method == "POST":
+        hash_and_salted_password = generate_password_hash(
+            request.form.get('password'),
+            method= 'pbkdf2:sha256',
+            salt_length=8
+        )
 
         new_user = User(
             email=request.form.get('email'),
