73d04e0aeb1681c34451008274a24f336d951877
Meliy <meyada.saisan@gmail.com>
2023-03-24 11:34:23 +0700

    add Gravatar images into comments section.

--- blog-with-users-start/main.py
+++ blog-with-users-start/main.py
@@ -23,6 +23,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 
